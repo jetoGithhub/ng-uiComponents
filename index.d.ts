204ce03319d88fe491f6fa52e8627c7c012fa0f7
Jefferson Lara <jefferson.arturo.lara.molina.sa@everis.com>
2017-12-07 12:09:28 -0500

    chore(module.ts): renombrando el module.ts y moviendolo al root de la aplicacion

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { TestComponentController } from './index';
+import { TestComponentController } from './src/index';
 
 @NgModule({
 	declarations:[
